738e7cb375f3a77f4a22a88adf2b3ae29612e8f0
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-03-20 17:11:21 -0300

    Add web page with streamlit

---
+++ web.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from conversor import polar_a_cartesiana, cartesiana_a_polar
+
+def main():
+    st.title("Conversor de Coordenadas")
+    conversion = st.selectbox("Selecciona la conversión", 
+                            ("Polar a Cartesiana", 
+                            "Cartesiana a Polar"))
+
+    if conversion == "Polar a Cartesiana":
+        r = st.number_input("Radio (r)", value=0.0)
+        theta = st.number_input("Ángulo (θ)", value=0.0)
+        x, y = polar_a_cartesiana(r, theta)
+        st.write("(X, Y) = ", (x, y))
+
+    elif conversion == "Cartesiana a Polar":
+        x = st.number_input("Coordenada X", value=0.0)
+        y = st.number_input("Coordenada Y", value=0.0)
+        r, theta = cartesiana_a_polar(x, y)
+        st.write("(r, theta) = ", (r, theta))
+
+if __name__ == "__main__":
+    main()
